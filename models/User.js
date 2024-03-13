5c5024c1df4e9b5f31cee14bdbe1022ada07dccd
salidamaharjan <salidamaharjan@outlook.com>
2024-03-13 16:20:33 -0500

    add email to user model with validator

--- models/User.js
+++ models/User.js
@@ -6,6 +6,17 @@ const userSchema = new Schema({
     unique: true,
     trim: true,
   },
+  email: {
+    type: String,
+    required: [true,'Email required'],
+    unique: true,
+    validate: {
+      validator: function(v){
+        return /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/.test(v);
+      },
+      message: "Please enter a valid email"
+    },
+  }
 });
 
 const User = model("user", userSchema);
