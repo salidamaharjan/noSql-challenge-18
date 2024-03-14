13568aeed2f798fe46d3ac83493665d12ba19393
salidamaharjan <salidamaharjan@outlook.com>
2024-03-14 16:27:59 -0500

    routes to get user by id

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,24 +1,32 @@
 const { Schema, Types } = require("mongoose");
 
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      timeStamps: true,
+    },
   },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxlength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    timeStamps: true,
-  },
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 module.exports = reactionSchema;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,6 +5,10 @@ router.get('/', async ( req, res) => {
     const user = await User.find();
     res.status(200).json(user);
 });
+router.get('/:id', async( req, res) => {
+    const userById = await User.findOne({_id: req.params.id });
+    !userById ? req.status(404).json({message: "No user found with that ID."}) : res.json(userById);
+});
 
 router.post('/', async (req, res) => {
     const user = await User.create(req.body);
