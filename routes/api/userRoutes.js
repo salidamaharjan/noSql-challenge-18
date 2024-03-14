e86c31d0fde9e14f0f1117b08ea0250d61f8d6d8
salidamaharjan <salidamaharjan@outlook.com>
2024-03-14 17:36:36 -0500

    create route to fine user with ID and update

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,18 +1,31 @@
-const router = require('express').Router();
-const { User} = require('../../models');
+const router = require("express").Router();
+const { User } = require("../../models");
 
-router.get('/', async ( req, res) => {
-    const user = await User.find();
-    res.status(200).json(user);
+router.get("/", async (req, res) => {
+  const user = await User.find();
+  res.status(200).json(user);
 });
-router.get('/:id', async( req, res) => {
-    const userById = await User.findOne({_id: req.params.id });
-    !userById ? req.status(404).json({message: "No user found with that ID."}) : res.json(userById);
+router.get("/:id", async (req, res) => {
+  const userById = await User.findOne({ _id: req.params.id });
+  !userById
+    ? req.status(404).json({ message: "No user found with that ID." })
+    : res.json(userById);
 });
 
-router.post('/', async (req, res) => {
-    const user = await User.create(req.body);
-    res.json(user);
+router.put("/:id", async (req, res) => {
+  const userById = await User.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: req.body },
+    { new: true }
+  );
+  !userById ? res.status(404).json({
+    message: 'No user with that ID',
+  }): res.json('User created');
+});
+
+router.post("/", async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
 });
 
 module.exports = router;
