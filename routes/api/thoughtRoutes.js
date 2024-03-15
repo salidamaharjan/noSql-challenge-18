df703b322d1576054f16cbc2a7bc0ba7d763fe37
salidamaharjan <salidamaharjan@outlook.com>
2024-03-14 22:17:01 -0500

    create route get thought by ID

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,6 +7,14 @@ router.get("/", async (req, res) => {
   res.status(200).json(thought);
 });
 
+//get the thought by Id
+router.get("/:id", async (req, res) => {
+  const thoughtById = await Thought.findOne({_id: req.params.id});
+  !thoughtById
+  ? req.status(404).json({ message: "No thought found with that ID." })
+  : res.json(thoughtById);
+})
+
 //create a new thought
 router.post("/", async (req, res) => {
   const thought = await Thought.create(req.body);
