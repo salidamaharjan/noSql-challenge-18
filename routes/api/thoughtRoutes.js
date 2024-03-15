ff1024f28f3497dbf4ebfc8adac10af2876fb562
salidamaharjan <salidamaharjan@outlook.com>
2024-03-14 22:12:35 -0500

    add comment to thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const { Thought } = require("../../models");
 
+//get all the thoughts
 router.get("/", async (req, res) => {
   const thought = await Thought.find();
   res.status(200).json(thought);
 });
 
+//create a new thought
 router.post("/", async (req, res) => {
   const thought = await Thought.create(req.body);
   res.status(200).json(thought);
