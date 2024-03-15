3603fd61115720f5de100bca3faa2481678c8de1
salidamaharjan <salidamaharjan@outlook.com>
2024-03-14 23:57:58 -0500

    create route to update the though by ID

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Thought, User } = require("../../models");
+const { findOneAndUpdate } = require("../../models/User");
 
 //get all the thoughts
 router.get("/", async (req, res) => {
@@ -26,6 +27,15 @@ router.post("/", async (req, res) => {
   !user
     ? res.status(404).json("Thought created, but no user found with that ID")
     : res.status(200).json(thought);
+});
+
+//update thoughts
+router.put("/:id", async (req, res) => {
+  const thoughtById = await Thought.findOneAndUpdate(
+    { _id: req.params.id },
+    {$set: { thoughtText: req.body.thoughtText }}
+  );
+  !thoughtById ? res.status(404).json("ID not found") : res.status(200).json("Thought updated!");
 });
 
 module.exports = router;
